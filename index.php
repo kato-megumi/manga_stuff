9f6604d3f27172764e6ab439767b5c511e0cbec1
komachi <29451351+kato-megumi@users.noreply.github.com>
2018-05-07 00:39:50 +0700

    nah

--- index.php
+++ index.php
@@ -1,46 +1,53 @@
 <!DOCTYPE html> 
 <html lang="en"> 
 <head> 
-   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
 </head>
 <style type="text/css">
 body {
-    background-color: black;
+  background-color: black;
 }
 
 h1 {
-    color: white;
-    text-align: center;
+  color: white;
+  text-align: center;
 }
 
 p {
-    font-family: verdana;
-    font-size: 20px;
+  font-family: verdana;
+  font-size: 20px;
 }
 img {max-width:100%; height:auto}
 </style>
 <body>
-   <center>
-      <?php
-      $root ="/mnt/tool/Project/xmanga/";
-      if(isset($_GET["dir"])){
-         $get = $_GET["dir"];
-         $dir = $root.$get;
-         $scan = scandir($dir);
-         for ($i=1; $i < count($scan)-1; $i++) { 
-            echo '<img src="/xmanga/'.$get.'/'.strval($i).'.jpg"><br>';
-         }
-         for ($i=1; $i < count($scan)-1; $i++) { 
-            echo '<img src="/xmanga/'.$get.'/'.strval($i).'.png"><br>';
-         }
-      } else {
-         $scan = scandir($root);
-         foreach ($scan as $v) {
-           echo '<a href="?dir='.$v.'">'.$v.'</a><br>';
-      }
-   }
-      ?> 
-   </center>
+  <center>
+   <?php
+   $dir =getcwd();
+   if(isset($_GET["dir"])){
+     $dir = $_GET["dir"];
+   }	 
+   $scan = scandir($dir);
+   $b = explode('/', $dir);
+   array_splice($b, count($b)-1);
+   $father = implode('/', $b);
+   echo '<a href="?dir='.$father.'">'."../ ( UP )".'</a><br>';
+   // echo '<a href="?dir='.$dir."../".'">'."../ ( UP() )".'</a><br>';
+   foreach ($scan as $v) {
+    $a = explode('.', $v);
+    $tail = $a[count($a)-1];
+    $full = $dir.'/'.$v;
+    if ($v != '.' && $v != ".." && is_dir($full)){
+      echo '<a href="?dir='.$full.'">'.$v.'</a><br>';
+    }
+    else {
+      echo $tail;
+    }
+    if ($tail == "jpg" || $tail == "png" ) {
+      echo '<img src="image.php?img='.$full.'" />';
+    }
+  }
+  ?> 
+</center>
 </body>
 
 </html>
